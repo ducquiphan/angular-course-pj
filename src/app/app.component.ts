a6ec0e22b6252a33f9db2caac5096ee3580de605
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2024-03-21 15:05:00 +0700

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
-import { AuthService } from './auth/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
 
-  ngOnInit() {
-    this.authService.autoLogin();
-  }
+    onAdd(item) {
+      this.list.push(item);
+    }
 }
